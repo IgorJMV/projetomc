ad0730a25feda8d5601e765b5a42b8ef6b24cd66
Igor Marinho <igorjmv2000@gmail.com>
2021-01-15 14:31:30 -0300

    Projeto criado

---
+++ src/main/java/com/igorjmv2000/gmail/ProjetomcApplication.java
@@ -0,0 +1,13 @@
+package com.igorjmv2000.gmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorjmv2000/gmail/ProjetomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorjmv2000.gmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

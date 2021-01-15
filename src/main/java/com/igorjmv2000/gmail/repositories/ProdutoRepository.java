1628e70747a324c126454f93c3b2f142dfa25281
Igor Marinho <igorjmv2000@gmail.com>
2021-01-15 17:15:37 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/igorjmv2000/gmail/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.igorjmv2000.gmail.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.igorjmv2000.gmail.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}

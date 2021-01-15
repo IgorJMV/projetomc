3cbad1af34d078b69f85839e3a61e6eda42fed6c
Igor Marinho <igorjmv2000@gmail.com>
2021-01-15 16:49:04 -0300

    Criando operação de instanciação

--- src/main/java/com/igorjmv2000/gmail/ProjetomcApplication.java
+++ src/main/java/com/igorjmv2000/gmail/ProjetomcApplication.java
@@ -1,13 +1,31 @@
 package com.igorjmv2000.gmail;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetomcApplication {
+import com.igorjmv2000.gmail.domain.Categoria;
+import com.igorjmv2000.gmail.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }

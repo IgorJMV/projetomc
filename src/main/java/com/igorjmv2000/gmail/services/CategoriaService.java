4114a5d036b81570e00c28041ee74514ed8acc59
Igor Marinho <igorjmv2000@gmail.com>
2021-01-16 14:33:08 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/igorjmv2000/gmail/services/CategoriaService.java
+++ src/main/java/com/igorjmv2000/gmail/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.igorjmv2000.gmail.domain.Categoria;
 import com.igorjmv2000.gmail.repositories.CategoriaRepository;
+import com.igorjmv2000.gmail.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,7 +18,8 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
 		
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
